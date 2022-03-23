00ddcc430ff927557232fb6542aca0acb522d7f9
Liptom328 <74043742+Liptom328@users.noreply.github.com>
2022-03-23 18:44:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ var heading = document.querySelector('.heading');
 var URLinput = document.querySelector('.URL-input');
 
 convertBtnMp3.addEventListener('click', () => {
-    heading.innerHTML = "Downloading...";
     sendURL(URLinput.value, 'mp3');
 });
 
 convertBtnMp4.addEventListener('click', () => {
-    heading.innerHTML = "Downloading...";
     sendURL(URLinput.value, 'mp4');
 });
 
 function sendURL(URL, format) {
     window.location.href = `https://adoredvictoriousengineers.liptom3281.repl.co/${format}?URL=${URL}`;
-    heading.innerHTML = "Liptom328 YouTube Downloader!";
 }
