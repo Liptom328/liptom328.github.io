d2f90a96a4477e468225aba55c39101feccd69dc
Liptom328 <74043742+Liptom328@users.noreply.github.com>
2022-03-25 18:54:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ var convertBtnMp4 = document.querySelector('.convert-button-mp4');
 var heading = document.querySelector('.heading');
 var URLinput = document.querySelector('.URL-input');
 
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
 convertBtnMp3.addEventListener('click', () => {
-    sendURL(URLinput.value, 'mp3');
+    sendURL(youtube_parser(URLinput.value), 'mp3');
 });
 
 convertBtnMp4.addEventListener('click', () => {
-    sendURL(URLinput.value, 'mp4');
+    sendURL(youtube_parser(URLinput.value), 'mp4');
 });
 
 function sendURL(URL, format) {
